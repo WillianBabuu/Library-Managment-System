7e2a5f73ada547a0babca6707b20eafffddf2694
Rose Kello <rose@DESKTOP-31R38CU>
2024-02-06 16:00:56 +0300

    created model for Categories

--- library-backend/src/main/java/com/kilimo/Library/category/Autowired.java
+++ library-backend/src/main/java/com/kilimo/Library/category/Autowired.java
@@ -1,5 +1,5 @@
 package com.kilimo.Library.category;
 
-public @interface Service {
+public @interface Autowired {
 
 }
--- library-backend/src/main/java/com/kilimo/Library/category/CategoryController.java
+++ library-backend/src/main/java/com/kilimo/Library/category/CategoryController.java
@@ -1,6 +1,7 @@
 package com.kilimo.Library.category;
 
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,16 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="api/v1/category")
 public class CategoryController {
 
+    /**
+     *
+     */
     private final CategoryService CategoryService;
+    private com.kilimo.Library.category.CategoryService categoryService;
 
+    /**
+     * @param categoryService
+     */
     @Autowired
 
     public CategoryController(CategoryService categoryService){
 
-        this.categoryService = clone()ategoryService;
+        CategoryService = categoryService;
+        this.categoryService = categoryService;
     }
 	public List<Category> getCategories() {
-        
+
 		return categoryService.getCategories();
 	}
 }
